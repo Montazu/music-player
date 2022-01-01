9f99331fdc3ee9de428684e7c1b5ebdd3eede839
Montazu <montazu@ya.ru>
2022-01-01 16:32:44 +0100

    optymalization code

--- script.js
+++ script.js
@@ -7,16 +7,12 @@ const icon = document.querySelectorAll(".music-player__button > svg");
 const playIcon = `<path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path>`;
 const pauseIcon = `<path d="M8 19c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2v10c0 1.1.9 2 2 2zm6-12v10c0 1.1.9 2 2 2s2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2z"></path>`;
 const setTime = (time, id) => {
-  const s = Math.floor(time % 60);
+  const s = Math.floor(Math.round(time) % 60);
   const m = Math.floor((time / 60) % 60);
   const handleTime = (unit, id) => {
     switch (id) {
-      case 0:
-        return unit != 0 ? (unit >= 10 ? `${unit}:` : `0${unit}:`) : "";
-        break;
       case 1:
         return unit != 0 ? (unit >= 10 ? unit : `0${unit}`) : 0;
-        break;
       default:
         return unit >= 10 ? `:${unit}` : `:0${unit}`;
     }
